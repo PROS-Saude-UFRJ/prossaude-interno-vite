d1b8f0dc5db3f1113737519c8f9f9ce00ff0ebb1
Aron Barbosa de Oliveira <aron.b.96@gmail.com>
2024-11-01 14:20:30 -0300

    Update TabInpSvi.tsx

--- app/components/interactive/edfis/client/tabs/TabInpSvi.tsx
+++ app/components/interactive/edfis/client/tabs/TabInpSvi.tsx
@@ -1,6 +1,6 @@
 import { TdProps } from "@/lib/global/declarations/interfaces";
 import { nlInp } from "@/lib/global/declarations/types";
-import { handleCondtReq, handleEventReq } from "@/lib/global/handlers/gHandlers";
+import { handleCondtReq } from "@/lib/global/handlers/gHandlers";
 import { useEffect, useRef, useState } from "react";
 import sEn from "@/styles//modules/enStyles.module.scss";
 export default function TabInpSvi({ nRow, nCol, ctx, lab }: TdProps): JSX.Element {
@@ -25,7 +25,7 @@ export default function TabInpSvi({ nRow, nCol, ctx, lab }: TdProps): JSX.Elemen
       inpRef.current.minLength = 1;
       inpRef.current.maxLength = 99;
       inpRef.current.dataset.min = "1";
-      inpRef.current.pattern = pattern;
+      inpRef.current.pattern = "^[0-9]+$";
       if (inpRef.current.type === "number") {
         inpRef.current.dataset.minNum = "0";
         inpRef.current.dataset.maxNum = "999999";
@@ -34,22 +34,19 @@ export default function TabInpSvi({ nRow, nCol, ctx, lab }: TdProps): JSX.Elemen
   }, [inpRef]);
   useEffect(() => {
     if (!inpRef.current) return;
-    if (inpRef.current.required) handleEventReq(inpRef.current);
-    else {
-      inpRef.current.type === "number"
-        ? handleCondtReq(inpRef.current, {
-            minNum: 0,
-            maxNum: 999999,
-            min: 1,
-            max: 99,
-            pattern: [pattern, ""],
-          })
-        : handleCondtReq(inpRef.current as HTMLInputElement, {
-            min: 1,
-            max: 99,
-            pattern: [pattern, ""],
-          });
-    }
+    inpRef.current.type === "number"
+      ? handleCondtReq(inpRef.current, {
+          minNum: 0,
+          maxNum: 999999,
+          min: 1,
+          max: 99,
+          pattern: [pattern, ""],
+        })
+      : handleCondtReq(inpRef.current as HTMLInputElement, {
+          min: 1,
+          max: 99,
+          pattern: [pattern, ""],
+        });
   }, [v, inpRef]);
   return (
     <input
@@ -57,7 +54,7 @@ export default function TabInpSvi({ nRow, nCol, ctx, lab }: TdProps): JSX.Elemen
       value={v}
       type='number'
       name={`${lab.toLowerCase()}_${nRow}_${nCol}`}
-      className={`form-control minNum minText maxText patternText tabInpProg tabInpProg${ctx} tabInpProg${lab}${ctx} tabInpRow${ctx}${nRow} float sevenCharLongNum ${sEn.tabInpProg}`}
+      className={`form-control tabInpProg tabInpProg${ctx} tabInpProg${lab}${ctx} tabInpRow${ctx}${nRow} float sevenCharLongNum ${sEn.tabInpProg}`}
       id={`tabInpRow${ctx}${nRow}_${nCol}`}
       min='0'
       max='65535'
